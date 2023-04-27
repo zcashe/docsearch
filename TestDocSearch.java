c43f6de5a85b6df88911dc31fee0c2c5a795bc2f
Dev Gulati <degulati@ucsd.edu>
2023-04-27 10:25:23 -0700

    updated test file for s23 data

--- TestDocSearch.java
+++ TestDocSearch.java
@@ -9,13 +9,13 @@ public class TestDocSearch {
 	public void testIndex() throws URISyntaxException, IOException {
     Handler h = new Handler("./technical/");
     URI rootPath = new URI("http://localhost/");
-    assertEquals("There are 224 total files to search.", h.handleRequest(rootPath));
+    assertEquals("There are 1391 total files to search.", h.handleRequest(rootPath));
 	}
 	@Test 
 	public void testSearch() throws URISyntaxException, IOException {
     Handler h = new Handler("./technical/");
-    URI rootPath = new URI("http://localhost/search?q=sushi");
-    String expect = "Found 3 paths:\n./written_2/travel_guides/berlitz1/WhatToJapan.txt\n./written_2/travel_guides/berlitz1/WhereToJapan.txt\n./written_2/travel_guides/berlitz2/California-WhereToGo.txt";
+    URI rootPath = new URI("http://localhost/search?q=Resonance");
+    String expect = "Found 2 paths:\n./technical/biomed/ar615.txt\n./technical/plos/journal.pbio.0020150.txt";
     assertEquals(expect, h.handleRequest(rootPath));
 	}
 }
