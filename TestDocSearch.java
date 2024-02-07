71c1b5dc36d5bc06fc9fa6feed9ac0a020853e79
Grace <45552424+yyygrace@users.noreply.github.com>
2024-02-07 12:02:45 -0800

    Update TestDocSearch.java

--- TestDocSearch.java
+++ TestDocSearch.java
@@ -1,4 +1,5 @@
 import static org.junit.Assert.*;
+import java.io.File;
 import org.junit.*;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -14,8 +15,9 @@ public class TestDocSearch {
 	@Test 
 	public void testSearch() throws URISyntaxException, IOException {
     Handler h = new Handler("./technical/");
+    String sep = File.separator;
     URI rootPath = new URI("http://localhost/search?q=Resonance");
-    String expect = "Found 2 paths:\n./technical/biomed/ar615.txt\n./technical/plos/journal.pbio.0020150.txt";
+    String expect = String.format("Found 2 paths:\n.%stechnical%sbiomed%sar615.txt\n.%stechnical%splos%sjournal.pbio.0020150.txt", sep, sep, sep, sep, sep, sep);
     assertEquals(expect, h.handleRequest(rootPath));
 	}
 }
